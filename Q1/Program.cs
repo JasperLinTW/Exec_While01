8a2b48c5c04a272557b2bfa890450bf1eb90a430
Jasper <guitar1859@gmail.com>
2022-10-23 23:31:17 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			/*Q1
+			1 + 2 + 3 + 4 + ....+N < 105
+			請問 N 是多少 ?*/
+			int i = 0;
+			int sum = 0;
+			while (sum < 105)
+			{
+				sum += i;
+				i++;
+			}
+			Console.WriteLine(i);
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int i = 1;
+			while (i <= 300)
+			{
+				if (300 % i == 0)
+				{
+					Console.WriteLine(i);
+				}
+				i++;
+			}
+		}
+	}
+}
